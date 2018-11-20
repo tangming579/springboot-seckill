2ffadff3d8cf607b28626cab585ac453b40c600c
Tangming <infly123@sina.com>
2018-11-20 20:07:20 +0800

    update

---
+++ src/main/java/com/tm/seckill/controller/SeckillController.java
@@ -0,0 +1,10 @@
+package com.tm.seckill.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller // @Service @Componet
+@RequestMapping("/seckill") // url:/模块/资源/{id}/细分 /seckill/list
+public class SeckillController {
+
+}
