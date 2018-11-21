18f0fe36681466ac754961f1677cb2ad7f99778a
Tangming <infly123@sina.com>
2018-11-21 20:32:34 +0800

    update

---
+++ src/main/java/com/tm/seckill/config/JedisConfig.java
@@ -0,0 +1,35 @@
+package com.tm.seckill.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+@Configuration
+public class JedisConfig extends CachingConfigurerSupport {
+
+    private Logger logger = LoggerFactory.getLogger(JedisConfig.class);
+
+    @Value("${spring.redis.host}")
+    private String host;
+
+    @Value("${spring.redis.port}")
+    private int port;
+
+    @Value("${spring.redis.timeout}")
+    private int timeout;
+
+    @Bean
+    public JedisPool redisPoolFactory(){
+        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+        JedisPool jedisPool = new JedisPool(jedisPoolConfig,host,port,timeout,null);
+
+        logger.info("JedisPool注入成功！");
+        logger.info("redis地址：" + host + ":" + port);
+        return  jedisPool;
+    }
+}
